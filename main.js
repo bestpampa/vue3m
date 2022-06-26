17884b845534f751eb88145943941b01182c3a18
bestpamp <aguspitarque@hotmail.com>
2022-06-26 14:21:30 -0300

    version 1

---
+++ main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            producto: 'Socks',
+            descripcion: '80% lana y 20% polyester',
+            image: './assets/images/socks_blue.jpg',
+            web: 'http://www.clarin.com',
+            InStock: true,
+            inventario: 0,
+            OnSale: true,
+            detalles: ['50% algodon', '22% seda', '18% papel', '10% fibra'],
+            talles: ['Large', "Medium", "Small"],
+            variantes: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'},
+            ]
+        }
+    }
+})

56c83c93ca0598d82a16613ac61440ec4763a4ad
bestpamp <aguspitarque@hotmail.com>
2022-06-28 00:00:04 -0300

    fin de la clase 7, falta el coding challenge

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ const app = Vue.createApp({
             descripcion: '80% lana y 20% polyester',
             image: './assets/images/socks_green.jpg',
             web: 'http://www.clarin.com',
-            InStock: true,
-            inventario: 0,
+            inStock: true,
+            activeClass: false,
+            inventario: 100,
             OnSale: true,
             cart: 0,
             carro: 0,
             detalles: ['50% algodon', '22% seda', '18% papel', '10% fibra'],
-            talles: ['Large', "Medium", "Small"],
+            talles: ['Large', "Medium", "Small", "XSmall"],
+            styles: {
+                color: 'red',
+                fontsize: '14px'
+            },
             variantes: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
